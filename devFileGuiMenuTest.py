7686a54358a1d564b752387049153ee4526f83a5
leoxie080808 <43616278+leoxie080808@users.noreply.github.com>
2021-08-07 20:30:45 -0400

    new drop down testing
    able to access the data of the drop down selection

--- devFileGuiMenuTest.py
+++ devFileGuiMenuTest.py
@@ -1,49 +1,35 @@
-from tkinter import ttk
-from tkinter import messagebox
-from tkinter import Tk
+import sys
+from PyQt5 import QtCore, QtWidgets
+from PyQt5.QtWidgets import QMainWindow, QLabel, QGridLayout, QWidget, QComboBox
+from PyQt5.QtCore import QSize, QRect
 
+class ExampleWindow(QMainWindow):
+    def __init__(self):
+        QMainWindow.__init__(self)
 
+        self.setMinimumSize(QSize(640, 140))
+        self.setWindowTitle("Combobox example")
 
-root = Tk()
+        centralWidget = QWidget(self)
+        self.setCentralWidget(centralWidget)
 
-root.geometry("400x400")
-#^ width - heghit window :D
+        # Create combobox and add items.
+        self.comboBox = QComboBox(centralWidget)
+        self.comboBox.setGeometry(QRect(40, 40, 491, 31))
+        self.comboBox.setObjectName(("comboBox"))
+        self.comboBox.addItem('printscreen', 1)
+        self.comboBox.addItem("alt-tab", 2)
+        self.comboBox.addItem("scrolling up", 3)
+        self.comboBox.addItem("scrolling down", 4)
 
+        self.comboBox.activated.connect(self.handleActivated)
 
-cmb = ttk.Combobox(root, width="10", values=("prova","ciao","come","stai"))
-#cmb = Combobox
+    def handleActivated(self, index):
+        print(self.comboBox.itemText(index))
+        print(self.comboBox.itemData(index))
 
-class TableDropDown(ttk.Combobox):
-    def __init__(self, parent):
-        self.current_table = tk.StringVar() # create variable for table
-        ttk.Combobox.__init__(self, parent)#  init widget
-        self.config(textvariable = self.current_table, state = "readonly", values = ["Customers", "Pets", "Invoices", "Prices"])
-        self.current(0) # index of values for current table
-        self.place(x = 50, y = 50, anchor = "w") # place drop down box 
-
-def checkcmbo():
-
-    if cmb.get() == "prova":
-        messagebox.showinfo("What user choose", "you choose prova")
-
-    elif cmb.get() == "ciao":
-        messagebox.showinfo("What user choose", "you choose ciao")
-
-    elif cmb.get() == "come":
-        messagebox.showinfo("What user choose", "you choose come")
-
-    elif cmb.get() == "stai":
-        messagebox.showinfo("What user choose", "you choose stai")
-
-    elif cmb.get() == "":
-        messagebox.showinfo("nothing to show!", "you have to be choose something")
-
-
-
-
-cmb.place(relx="0.1",rely="0.1")
-
-btn = ttk.Button(root, text="Get Value",command=checkcmbo)
-btn.place(relx="0.5",rely="0.1")
-
-root.mainloop()
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    mainWin = ExampleWindow()
+    mainWin.show()
+    sys.exit( app.exec_() )
